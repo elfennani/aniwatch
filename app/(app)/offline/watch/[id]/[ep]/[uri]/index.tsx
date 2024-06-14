94da5c59727749c82a7fd36f7f9f6f899f9f9281
Elfennani Nizar <elfennani2002@gmail.com>
2024-06-14 21:53:09 +0100

    added keep awake, and fixed updateEntry

--- app/(app)/offline/watch/[id]/[ep]/[uri]/index.tsx
+++ app/(app)/offline/watch/[id]/[ep]/[uri]/index.tsx
@@ -4,13 +4,15 @@ import { StatusBar } from "expo-status-bar";
 import Player from "@/components/player";
 import useUpdateEntry from "@/hooks/use-update-entry";
 import useShowQuery from "@/api/use-show-query";
+import { useKeepAwake } from "expo-keep-awake";
 
 type Params = { uri: string; id: string; ep: string };
 
 const OfflineWatchPage = () => {
+  useKeepAwake();
   const { uri, ep, id } = useLocalSearchParams<Params>();
   const { data: media } = useShowQuery({ id: Number(id) });
-  const updateEntry = useUpdateEntry(Number(ep), Number(ep), media!);
+  const updateEntry = useUpdateEntry(Number(ep), Number(id), media!);
 
   return (
     <>
